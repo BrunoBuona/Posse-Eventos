00263d99ce343d27bd17662b0a7e666e19f43a0a
Bruno Buonassisa <108221247+BrunoBuona@users.noreply.github.com>
2023-07-03 17:50:42 -0300

    Add files via upload

---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import './Footer.css'
+import { SocialIcon } from 'react-social-icons';
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+
+function Footer() {
+    const {t,i18n} = useTranslation()
+    return (
+        <footer className="footer">
+                <h2 className="other-titles-footer">{t('¡Nuestras Redes!')}</h2>
+                <div className="container-btns-footer">
+                    
+                    <button className="footer-social-btns">
+                        <SocialIcon className="icon-social" network="instagram" bgColor="#9F00FF" fgColor="#ffffff" style={{ height: 40, width: 40 }} />
+                        <h6 className="footer-social-text">Instagram</h6>
+                    </button>
+                    <button className="footer-social-btns">
+                        <SocialIcon className="icon-social" network="twitter" bgColor="#9F00FF" fgColor="#ffffff" style={{ height: 40, width: 40 }} />
+                        <h6 className="footer-social-text">Twitter</h6>
+                    </button>
+                </div>
+
+                <div>
+                    <h4 className="footer-text">© 2023 TuMarca. Todos los derechos reservados.</h4>
+                </div>
+        </footer>
+    )
+}
+export { Footer }
