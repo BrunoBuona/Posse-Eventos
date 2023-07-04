4763e07d084a36defeb9f0d0cfec7267983c7f8c
BrunoBuona <brunobuonassisa@gmail.com>
2023-07-04 19:58:44 -0300

    styles changes

--- src/pages/AdminHome/AdminHome.jsx
+++ src/pages/AdminHome/AdminHome.jsx
@@ -5,41 +5,41 @@ export default function AdminHome() {
   return (
     <div>
       <h1>Bienvenida al panel de Administración.</h1>
-      <table >
-        <thead>
-          <tr>
-            <th>Sección</th>
-            <th>Descripción</th>
+      <table className="table2">
+        <thead className="thead2">
+          <tr className="tr2">
+            <th className="td2">Sección</th>
+            <th className="td2">Descripción</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Inicio</td>
-            <td>
+          <tr className="tr2">
+            <td className="td2">Inicio</td>
+            <td className="td2">
               En esta sección podrás ver la información sobre el panel de administración.
             </td>
           </tr>
-          <tr>
-            <td>Eventos</td>
-            <td>
+          <tr className="tr2">
+            <td className="td2">Eventos</td>
+            <td className="td2">
               En esta sección podrás ver, crear, editar y eliminar eventos.
             </td>
           </tr>
-          <tr>
-            <td>Artistas</td>
-            <td>
+          <tr className="tr2">
+            <td className="td2">Artistas</td>
+            <td className="td2">
               En esta sección podrás ver, crear, editar y eliminar artistas.
             </td>
           </tr>
-          <tr>
-            <td>Bebidas</td>
-            <td>
+          <tr className="tr2">
+            <td className="td2">Bebidas</td>
+            <td className="td2">
               En esta sección podrás ver, crear, editar y eliminar bebidas.
             </td>
           </tr>
-          <tr>
-            <td>Localizaciones</td>
-            <td>
+          <tr className="tr2">
+            <td className="td2">Localizaciones</td>
+            <td className="td2">
               En esta sección podrás ver, crear, editar y eliminar
               localizaciones.
             </td>
