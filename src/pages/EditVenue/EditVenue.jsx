f391bfae75b5d86eb190baf3b4aea6ccea0f5a25
BrunoBuona <brunobuonassisa@gmail.com>
2023-07-11 06:26:54 -0300

    fix adress URL

--- src/pages/EditVenue/EditVenue.jsx
+++ src/pages/EditVenue/EditVenue.jsx
@@ -88,8 +88,8 @@ export default function EditVenue() {
                 <Field name="name" placeholder="Rancho de la V" value={values.name} />
               </div>
               <div>
-                <label htmlFor="address">Dirección de la Localización:</label>
-                <Field name="address" placeholder="KM 11 - 22" value={values.address} />
+                <label htmlFor="urlAddress">Dirección de la Localización:</label>
+                <Field name="urlAddress" placeholder="KM 11 - 22" value={values.address} />
               </div>
               <div>
               <label htmlFor="address">URL de la Dirección:</label>
